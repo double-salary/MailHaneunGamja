1d7d41c46d1a9f387e2e55b74c15a8bd305863ce
Subeen Park <psb021414@gmail.com>
2021-08-10 06:38:58 +0900

    feat: landing page

--- svelte-app/src/components/HeaderBarBrand.svelte
+++ svelte-app/src/components/HeaderBarBrand.svelte
@@ -1,112 +1,19 @@
-<style>
-  .new_navbar {
-    width: 70%;/*new_navbar와 new_login 7:3으로 width 분할(화면좁아서 개행되면 width 100%로 사용)*/
-    display: flex;
-    justify-content: space-evenly; /*적당히 가운데 몰리게*/
-    margin-top: 18px;
-  }
-  
-  .new_navbar a {
-    float: left;
-    font-size: 16px;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
-  
-  .new_dropdown {
-    display: flex;
-    flex-direction: column;
-    justify-content: center; /*가운데정렬*/
-  }
-  
-  .new_dropdown .new_dropbtn {
-    font-size: 16px;  
-    border: none;
-    outline: none;
-    color: black;
-    padding: 14px 16px;
-    background-color: inherit;
-    font-family: inherit;
-    margin: 0;
-  }
-  
-  .new_dropdown-content {
-    display: none;
-    position: absolute;
-    margin-top: 180px; /*메뉴 hover했을때 하단 메뉴가 바로 아래에 위치하게 하는 용도*/
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-  
-  .new_dropdown-content a {
-    float: none;
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-  }
-  
-  .new_dropdown-content a:hover {
-    background-color: #ddd; /*뒷배경 넣어도 여기는 회색처리 해야할듯*/
-  }
-  
-  .new_dropdown:hover .new_dropdown-content {
-    display: block;
-  }
-
-  .new_login{
-    width: 30%; /*new_navbar와 new_login 7:3으로 width 분할(화면좁아서 개행되면 100%로 사용)*/
-    margin-top: 18px;
-    display: flex;
-    justify-content: center; /*가운데정렬*/
-  }
-
-  .new_login a{
-    font-size: 16px;
-    color: black;
-    display: flex;
-    flex-direction: column;
-    justify-content: center; /*가운데정렬*/
-  }
-
-  /*new_navbar와 new_login 화면좁아서 개행되면 width 100%로 사용)*/
-  @media screen and (max-width: 1024px) {
-    .new_login{
-      width: 100%;
-    }
-    .new_navbar {
-      width: 100%;
-    }
-  }
-</style>
-
-
 <div class="new_navbar">
   <div class="new_dropdown">
     <button class="new_dropbtn">
-      <img src="./logo.png" alt="멜하는감자" style="height: 40px"/> <!--이미지 경로 기준은 public 폴더-->
+      <img
+        src="./resources/img/logo.png"
+        alt="멜하는감자"
+        style="height: 40px"
+      />
+      <!--이미지 경로 기준은 public 폴더-->
     </button>
   </div>
 
   <div class="new_dropdown">
-    <button class="new_dropbtn">성적 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="new_dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div>
-
-  <div class="new_dropdown">
-    <button class="new_dropbtn">수업 
-      <i class="fa fa-caret-down"></i>
+    <button class="new_dropbtn"
+      >성적
+      <i class="fa fa-caret-down" />
     </button>
     <div class="new_dropdown-content">
       <a href="#">Link 1</a>
@@ -116,8 +23,9 @@
   </div>
 
   <div class="new_dropdown">
-    <button class="new_dropbtn">수강신청 
-      <i class="fa fa-caret-down"></i>
+    <button class="new_dropbtn"
+      >수업
+      <i class="fa fa-caret-down" />
     </button>
     <div class="new_dropdown-content">
       <a href="#">Link 1</a>
@@ -127,8 +35,21 @@
   </div>
 
   <div class="new_dropdown">
-    <button class="new_dropbtn">기타 
-      <i class="fa fa-caret-down"></i>
+    <button class="new_dropbtn"
+      >수강신청
+      <i class="fa fa-caret-down" />
+    </button>
+    <div class="new_dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+
+  <div class="new_dropdown">
+    <button class="new_dropbtn"
+      >기타
+      <i class="fa fa-caret-down" />
     </button>
     <div class="new_dropdown-content">
       <a href="#">Link 1</a>
@@ -138,5 +59,93 @@
   </div>
 </div>
 <div class="new_login">
-  <a href="/.auth/login/google?post_login_redirect_uri=/products/">로그인 / 회원가입</a>
+  <a href="/.auth/login/google?post_login_redirect_uri=/products/"
+    >로그인 / 회원가입</a
+  >
 </div>
+
+<style>
+  .new_navbar {
+    width: 70%; /*new_navbar와 new_login 7:3으로 width 분할(화면좁아서 개행되면 width 100%로 사용)*/
+    display: flex;
+    justify-content: space-evenly; /*적당히 가운데 몰리게*/
+    margin-top: 18px;
+  }
+
+  .new_navbar a {
+    float: left;
+    font-size: 16px;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  .new_dropdown {
+    display: flex;
+    flex-direction: column;
+    justify-content: center; /*가운데정렬*/
+  }
+
+  .new_dropdown .new_dropbtn {
+    font-size: 16px;
+    border: none;
+    outline: none;
+    color: black;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+  }
+
+  .new_dropdown-content {
+    display: none;
+    position: absolute;
+    margin-top: 180px; /*메뉴 hover했을때 하단 메뉴가 바로 아래에 위치하게 하는 용도*/
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+  }
+
+  .new_dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+  }
+
+  .new_dropdown-content a:hover {
+    background-color: #ddd; /*뒷배경 넣어도 여기는 회색처리 해야할듯*/
+  }
+
+  .new_dropdown:hover .new_dropdown-content {
+    display: block;
+  }
+
+  .new_login {
+    width: 30%; /*new_navbar와 new_login 7:3으로 width 분할(화면좁아서 개행되면 100%로 사용)*/
+    margin-top: 18px;
+    display: flex;
+    justify-content: center; /*가운데정렬*/
+  }
+
+  .new_login a {
+    font-size: 16px;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center; /*가운데정렬*/
+  }
+
+  /*new_navbar와 new_login 화면좁아서 개행되면 width 100%로 사용)*/
+  @media screen and (max-width: 1024px) {
+    .new_login {
+      width: 100%;
+    }
+    .new_navbar {
+      width: 100%;
+    }
+  }
+</style>
---
+++ svelte-app/src/landings/Landings.svelte
@@ -0,0 +1,235 @@
+<script>
+  import Typewriter from './Typewriter.svelte';
+  document.body.classList.add('disable-scroll');
+  let innerWidth = 1600;
+  let innerHeight = 1024;
+
+  $: leftCircleDiameter = Math.min(0.55 * innerWidth, 0.86 * innerHeight);
+  $: leftCircleRadius = leftCircleDiameter / 2;
+  $: rightCircleDiameter = Math.min(0.58 * innerWidth, 0.88 * innerHeight);
+  $: rightCircleRadius = rightCircleDiameter / 2;
+  $: rightCircleOffsetX = -0.13 * innerWidth;
+  $: rightCircleOffsetY = -0.2 * innerHeight;
+  $: leftCircleOffsetX = -0.02 * innerWidth;
+  $: leftCircleOffsetY = -0.29 * innerHeight;
+
+  $: leftDivWidth = Math.min(480, (innerWidth * 5) / 12);
+  $: rightDivWidth = Math.min(684, (innerWidth * 7) / 12);
+  $: divHeight = innerHeight * 0.6;
+  $: leftCircleOffsetY = -0.29 * innerHeight;
+  $: fontSize = Math.round(0.12 * leftDivWidth);
+</script>
+
+<svelte:window bind:innerWidth bind:innerHeight />
+
+<div class="background-layer">
+  <svg
+    style="
+    height: {leftCircleDiameter};
+    width: {leftCircleDiameter};
+    position: fixed;
+    bottom: {leftCircleOffsetY};
+    left: {leftCircleOffsetX};
+    "
+    class="background-layer"
+  >
+    <circle
+      cx={leftCircleRadius}
+      cy={leftCircleRadius}
+      r={leftCircleRadius}
+      fill="var(--green-background)"
+      class="background-layer"
+    />
+  </svg>
+</div>
+
+<!-- background layer: circles -->
+<div class="background-layer">
+  <svg
+    style="
+      height: {rightCircleDiameter};
+      width: {rightCircleDiameter};
+      position: fixed;
+      top: {rightCircleOffsetY};
+      right: {rightCircleOffsetX};
+      "
+    class="background-layer"
+  >
+    <circle
+      cx={rightCircleRadius}
+      cy={rightCircleRadius}
+      r={rightCircleRadius}
+      fill="var(--yellow-background)"
+      class="background-layer"
+    />
+  </svg>
+</div>
+
+<div class="flex-container">
+  <div class="main-layer">
+    <div class="left-div" style:width={leftDivWidth}>
+      <div
+        class="text-slogan"
+        style="font-size: {fontSize}px; width: {leftDivWidth};"
+      >
+        메일 앞에서 <br /> 말하는 감자가 <br /> 되어버리는 <br /> 당신을 위해서
+      </div>
+    </div>
+    <div class="right-div" style="width: {rightDivWidth}; height: {divHeight};">
+      <div class="top-bar">
+        <div class="circle circle-1" />
+        <div class="circle circle-2" />
+        <div class="circle circle-3" />
+      </div>
+      <div class="to">
+        <div class="field">To:</div>
+        <div class="to-prof">
+          존경하는 교수님께
+          <img src="../resources/img/close.png" alt="close button" />
+        </div>
+      </div>
+
+      <div class="title">Title: 제목제목제목제목</div>
+      <div class="text">
+        <Typewriter>
+          <div>존경하는 교수님께, </div>
+          <div>안녕하세요 교수님, 박아무개입니다.</div>
+          <div>다름이 아니오라...</div>
+        </Typewriter>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- main layer: typos/mail window design -->
+<style>
+  .background-layer {
+    z-index: -1;
+  }
+
+  .flex-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .main-layer {
+    display: flex;
+    flex-direction: row;
+    max-width: 1200px;
+    width: 100%;
+  }
+
+  .left-div {
+    margin: 0;
+    width: 40%;
+  }
+
+  .text-slogan {
+    font-family: 'GmarketSansLight';
+    width: 100%;
+    margin: 0;
+  }
+
+  .right-div {
+    width: 60%;
+    background-color: white;
+    border-radius: 16px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .top-bar {
+    height: 40px;
+    background-color: var(--lightGray-background);
+    border-radius: 16px 16px 0px 0px;
+    padding-left: 17px;
+    width: 100%;
+    float: left;
+  }
+
+  .circle {
+    z-index: 10;
+    width: 14px;
+    height: 14px;
+    border-radius: 50%;
+    display: inline-block;
+    margin: 13px 3px;
+  }
+
+  .circle-1 {
+    background-color: var(--purple-main);
+  }
+
+  .circle-2 {
+    background-color: var(--green-main);
+  }
+
+  .circle-3 {
+    background-color: var(--yellow-main);
+  }
+
+  .to {
+    float: left;
+    height: 40px;
+    border-bottom: 2px solid var(--lightGray-background);
+    width: 100%;
+    vertical-align: middle;
+  }
+
+  .field {
+    font-family: 'NanumSquare', sans-serif !important;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 40px;
+    color: var(--blueGray-text);
+    display: inline-block;
+    padding-left: 11px;
+  }
+
+  .to-prof {
+    display: inline-block;
+    margin-left: 15px;
+    font-family: 'NanumSquare', sans-serif !important;
+    font-size: 12px;
+    font-weight: bold;
+    background-color: var(--purple-main);
+    color: white;
+    border-radius: 8px;
+    padding: 3px 8px 3px 8px;
+    position: relative;
+    top: -2px;
+  }
+
+  .to-prof img {
+    width: 12px;
+    height: 12px;
+    padding: 1px;
+    position: relative;
+    top: 2px;
+  }
+
+  .title {
+    height: 40px;
+    width: 100%;
+    font-family: 'NanumSquare', sans-serif !important;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 40px;
+    color: var(--blueGray-text);
+    padding-left: 11px;
+    border-bottom: 2px solid var(--lightGray-background);
+    margin-bottom: 0px;
+  }
+
+  .text {
+    font-family: 'NanumSquare', sans-serif !important;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 28px;
+    margin-top: 22px;
+    margin-left: 24px;
+  }
+</style>
