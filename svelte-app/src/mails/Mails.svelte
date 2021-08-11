d1f12fb2b9c0cbcac1b2f2cd0fc39e3e405e5db3
seoljiwon <suljiwon@naver.com>
2021-08-11 13:10:49 +0900

    Feat: Mails page svelte, css

--- svelte-app/src/mails/Mails.svelte
+++ svelte-app/src/mails/Mails.svelte
@@ -1,8 +1,27 @@
 <script>
+  import Sidebar from './SideBar.svelte';
+  import Mail from './Mail.svelte';
+  import css from './Mail.css';
+
+  const data = {
+    평가방식문의: ['퀴즈 평가', '성적 평가'],
+    과제문의: [
+      '지각제출/재제출',
+      '최종 자료 제출',
+      '과제 내용',
+      '과제 마감기한',
+    ],
+    출결문의: ['공결/병결/개인 사유', '녹강/배속'],
+  };
 </script>
 
-<div class="content-container">
-  <div class="side-bar" />
-  <div class="mail-content" />
-  <div class="example" />
+<div class="container">
+  <div class="side-bar">
+    {#each Object.entries(data) as entry}
+      <Sidebar {entry} />
+    {/each}
+  </div>
+  <div class="mail">
+    <Mail />
+  </div>
 </div>
