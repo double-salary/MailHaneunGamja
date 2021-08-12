ad398964447a48104ce3ba9566c13f1f2f61855f
Subeen Park <psb021414@gmail.com>
2021-08-12 22:26:22 +0900

    fix: .gitignore api removed

---
+++ api/accounts/index.js
@@ -0,0 +1,110 @@
+console.log("INDEX.JS APPROACHED")
+const auth = require('../shared/get-user-info.js');
+
+// module.exports = async function (context, req) {
+//     context.log('JavaScript HTTP trigger function processed a request.');
+
+//     const name = (req.query.name || (req.body && req.body.name));
+//     const responseMessage = name
+//         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+//         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+//     context.res = {
+//         // status: 200, /* Defaults to 200 */
+//         body: responseMessage
+//     };
+// }
+
+
+// Load mongoose
+const mongoose = require('mongoose');
+
+// Connect to the database
+mongoose.connect(
+    process.env.CONNECTION_STRING, // Retrieve connection string
+    { // boiler plate values
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+// Create the schema or structure of our object in Mongoose
+const userDataSchema = new mongoose.Schema({
+
+    userId: String,
+    department: String, 
+    studentId: String,
+    bookmarks: [String] // ["courses/exception/1"]
+    
+});
+
+// Create a model using our schema
+// This model will be used to access the database
+const userDataModel = mongoose.model('userData', userDataSchema);
+
+// Export our function
+module.exports = async function (context, req) {
+    // setup our default content type (we always return JSON)
+    context.res = {
+        header: {
+            "Content-Type": "application/json"
+        }
+    }
+
+
+    // Read the method and determine the requested action
+    switch (req.method) {
+        // If get, return all tasks
+        case 'GET':
+            await getProfile(context);
+            break;
+        // If post, create new task
+        case 'POST':
+            await createProfile(context);
+            break;
+        // If put, update task
+        case 'PUT':
+            await updateProfile(context);
+            break;
+    }
+};
+
+// Return all tasks
+async function getProfile(context) {
+    // load all tasks from database
+    // const tasks = await userDataModel.find({ userId: auth.getUserInfo(). });
+    // return all tasks
+    console.log("getProfile")
+    console.log(auth.getUserInfo())
+    context.res.body = { tasks: tasks };
+}
+
+// Create new task
+async function createTask(context) {
+    // Read the uploaded task
+    const body = context.req.body;
+    // Save to database
+    const task = await TaskModel.create(body);
+    // Set the HTTP status to created
+    context.res.status = 201;
+    // return new object
+    context.res.body = task;
+}
+
+// Update an existing function
+async function updateTask(context) {
+    // Grab the id from the URL (stored in bindingData)
+    const id = context.bindingData.id;
+    // Get the task from the body
+    const task = context.req.body;
+    // Update the item in the database
+    const result = await TaskModel.updateOne({ _id: id }, task);
+    // Check to ensure an item was modified
+    if (result.nModified === 1) {
+        // Updated an item, status 204 (empty update)
+        context.res.status = 204;
+    } else {
+        // Item not found, status 404
+        context.res.status = 404;
+    }
+}
