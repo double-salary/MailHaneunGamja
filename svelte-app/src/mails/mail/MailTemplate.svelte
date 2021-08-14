e1392762defe56959081658fa963162c54d37153
Subeen Park <psb021414@gmail.com>
2021-08-15 00:39:31 +0900

    fix: merge conflict

--- svelte-app/src/mails/mail/MailTemplate.svelte
+++ svelte-app/src/mails/mail/MailTemplate.svelte
@@ -22,12 +22,6 @@
   var bookmarked = false;
   let hideBookmark = true;
 
-  const exampleData = [
-    '1예시예시 슬슬 디자인이 귀찮아지기 시작해씅ㅁ 구구절절 구구절절',
-    '2예시예시 슬슬 디자인이 귀찮아지기 시작해씅ㅁ 구구절절 구구절절',
-    '3예시예시 슬슬 디자인이 귀찮아지기 시작해씅ㅁ 구구절절 구구절절',
-  ];
-
   onMount(async () => {
     lastWords = getRandomLastWords(checkedWeather);
     const userInfo = await getUserAction();
@@ -135,7 +129,7 @@
       <!-- 사유 예시 -->
       <div class="mail__example">
         <div class="mail__example-header">
-          <input class="mail__reason" value="사유" readonly/>
+          <input class="mail__reason" value="사유" readonly />
         </div>
         <p
           placeholder="다름이 아니라 저번학기에 이어 제가 이번학기에도 컴퓨터의 개념 및 실습의 수강 신청에 실패했는데, 이번학기에도 듣지 못한다면 계속해서 이후 수강신청에도 차질이 생길 것 같습니다."
